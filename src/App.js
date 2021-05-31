329d897965b452ea7d98ba85e5c0149164ab3288
sitd <tdisivip98@gmail.com>
2021-05-31 22:29:40 +0700

    update register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import Login from '../src/SiTD/Login';
-
+//import Login from '../src/SiTD/Login';
+import Register from '../src/SiTD/Register';
 function App() {
   return (
     <div className="App">
-      <Login/>
+      <Register/>
       
     </div>
   );
---
+++ src/SiTD/Register.js
@@ -0,0 +1,17 @@
+
+
+
+function Register() {
+  return (
+    <div className="Register">
+     
+        <div>Đây là trang Register</div>
+        <h1>Xin chào nè</h1>    
+      
+        <div>Đây là trang login chào anh sỹ đẹp trai</div>
+     
+    </div>
+  );
+}
+
+export default Register;
